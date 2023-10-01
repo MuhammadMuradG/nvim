21e37bdfbf5f86dc66c53ebf7d325c4fcf0133c4
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2023-10-01 23:02:31 +0200

    Update indent-blankline config to v3

--- lua/plugins-config/blankline.lua
+++ lua/plugins-config/blankline.lua
@@ -1,17 +1,8 @@
-require("indent_blankline").setup {
-    char = "│",
-	show_first_indent_level = true,
-
-	use_treesitter = true,
-
-    buftype_exclude = {
-		"terminal",
-	},
-
-	filetype_exclude = {
-		'help',
-		'startify',
-		'NvimTree',
-		'packer',
+require("ibl").update({
+	exclude = {
+		filetypes = {
+			"startify",
+			"NvimTree",
+		},
 	}
-}
+})
