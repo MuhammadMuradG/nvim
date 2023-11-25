bd54c8f8fcec68d009b4e37e32d779ead13b4bf1
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2023-11-25 14:17:58 +0200

    Replace using deprecated plugin manager with pckr

--- init.lua
+++ init.lua
@@ -1,64 +1,70 @@
---------------------------------------------------------------------------------
--- packer plugin Manager settings
---------------------------------------------------------------------------------
-local packer = require('packer')
+-- Install pkcr automatically
+local function bootstrap_pckr()
+	local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
 
-packer.init(
-	{
-		git = {
-			clone_timeout = 120, -- Timeout, in seconds, for git clones
-		},
-	}
-)
-
-packer.startup(
-	function()
-		local use = use
-		-- Packer can manage itself
-		use 'wbthomason/packer.nvim'
-
-		-- opt stand for optional so it will loaded on-demand with the :packadd command.
-		-- the following package will loaded only if file type is markdown.
-		-- use {'iamcco/markdown-preview.nvim', run = 'cd app && npm install', ft = {'markdown'}}
-
-		-- Install directory explorer plugin
-		use {"nvim-tree/nvim-tree.lua", requires = {"nvim-tree/nvim-web-devicons"}}
-
-		-- Autocompletion plugins and LSP support
-		use {'neoclide/coc.nvim', branch = 'release'}
-		use 'honza/vim-snippets'                                -- Provide snippets for coc-snippets
-
-		-- Appearance and themes
-		use 'itchyny/lightline.vim'
-		use 'vim-ctrlspace/vim-ctrlspace'
-		use 'mhinz/vim-startify'
-		use 'ryanoasis/vim-devicons'                            -- Provided graphical icon
-		use 'sainnhe/artify.vim'                                -- Provided customizable font
-		use 'sainnhe/gruvbox-material'
-		use 'morhetz/gruvbox'
-		use 'sainnhe/everforest'
-		use 'sainnhe/edge'
-
-		-- REPL and Debugger plugins
-		use {'puremourning/vimspector', branch = 'master'}      -- Debugger plugin
-
-		-- Helper plugins for productivity
-		use {'iamcco/markdown-preview.nvim', run = 'cd app && npm install'}
-		use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-		use 'tpope/vim-fugitive'                                -- The premier Git plugin for Vim, it is illegal.
-		use 'norcalli/nvim-colorizer.lua'
-		use 'terrortylor/nvim-comment'
-		use 'AndrewRadev/splitjoin.vim'
-		use 'matze/vim-move'
-		use "Pocco81/auto-save.nvim"
-		use 'lukas-reineke/indent-blankline.nvim'
+	if not vim.loop.fs_stat(pckr_path) then
+		vim.fn.system({
+			'git',
+			'clone',
+			"--filter=blob:none",
+			'https://github.com/lewis6991/pckr.nvim',
+			pckr_path
+		})
 	end
-)
+
+	vim.opt.rtp:prepend(pckr_path)
+end
+
+bootstrap_pckr()
 
 
--- Set Options and Keymaps
+-- Install plugins
+require('pckr').add{
+	-- Install directory explorer plugin
+	{"nvim-tree/nvim-tree.lua", requires = {"nvim-tree/nvim-web-devicons"}};
+
+	-- Autocompletion plugins and LSP support
+	{'neoclide/coc.nvim', branch = 'release'};
+	'honza/vim-snippets';                                -- Provide snippets for coc-snippets
+
+	-- Appearance and themes
+	'itchyny/lightline.vim';
+	'vim-ctrlspace/vim-ctrlspace';
+	'mhinz/vim-startify';
+	'ryanoasis/vim-devicons';                            -- Provided graphical icon
+	'sainnhe/artify.vim';                                -- Provided customizable font
+	'sainnhe/gruvbox-material';
+	'morhetz/gruvbox';
+	'sainnhe/everforest';
+	'sainnhe/edge';
+
+	-- REPL and Debugger plugins
+	{'puremourning/vimspector', branch = 'master'};      -- Debugger plugin
+
+	-- Helper plugins for productivity
+	{'iamcco/markdown-preview.nvim', run = 'cd app && npm install'};
+	{'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'};
+	'tpope/vim-fugitive';                                -- The premier Git plugin for Vim, it is illegal.
+	'norcalli/nvim-colorizer.lua';
+	'terrortylor/nvim-comment';
+	'AndrewRadev/splitjoin.vim';
+	'matze/vim-move';
+	"Pocco81/auto-save.nvim";
+	'lukas-reineke/indent-blankline.nvim';
+}
+
+-------------------------------------------------------------------------------
+-- Configurations
+-------------------------------------------------------------------------------
 require('options')
 require('keymaps')
+
+-- Load vimscript config
+vim.cmd('source ~/.config/nvim/vim/lightline.vim')
+vim.cmd('source ~/.config/nvim/vim/vim-startify.vim')
+vim.cmd('source ~/.config/nvim/vim/coc-nvim.vim')
+vim.cmd('source ~/.config/nvim/vim/vimspector.vim')
+vim.cmd('source ~/.config/nvim/vim/markdown.vim')
 
 -- Load lua plugins configurations
 require('plugins-config/ts')
@@ -69,10 +75,3 @@ require('plugins-config/colorizer')
 require('plugins-config/blankline')
 require('plugins-config/nvim-comment')
 require('plugins-config/auto-save')
-
--- Load vimscript config
-vim.cmd('source ~/.config/nvim/vim/lightline.vim')
-vim.cmd('source ~/.config/nvim/vim/vim-startify.vim')
-vim.cmd('source ~/.config/nvim/vim/coc-nvim.vim')
-vim.cmd('source ~/.config/nvim/vim/vimspector.vim')
-vim.cmd('source ~/.config/nvim/vim/markdown.vim')
