0e06f11622caf88e2a572815a872010163755d15
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2023-11-25 14:18:21 +0200

    Fix theme configs.

--- lua/plugins-config/theme-config.lua
+++ lua/plugins-config/theme-config.lua
@@ -1,15 +1,13 @@
 -- For dark or light scheme
 vim.opt.background = 'dark'                      -- dark or light
 
-vim.g.gruvbox_material_background = 'soft'       -- hard, soft or medium
 vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_disable_italic_comment = 0
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_cursor = 'auto'
-vim.g.gruvbox_material_palette = 'material'      -- Available choice material, mix, original
+vim.g.gruvbox_material_palette = 'material'      -- Available choices: material, mix, original
 
--- This configuration option should be placed before `colorscheme gruvbox-material`
--- available values: 'hard', 'medium'(default), 'soft'
-vim.g.gruvbox_material_bakground = 'soft'
+-- This option should be placed before `colorscheme gruvbox-material`
+vim.g.gruvbox_material_background = 'hard'       -- Available values: 'hard', 'medium'(default), 'soft'
 
-vim.cmd('colorscheme gruvbox-material')          -- Available choice gruvbox-material, everforest, edge or gruvbox
+vim.cmd('colorscheme gruvbox-material')          -- Available choices: gruvbox-material, everforest, edge or gruvbox
