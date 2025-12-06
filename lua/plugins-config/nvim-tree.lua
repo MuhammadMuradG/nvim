a1ceea7076c8113a6a3c300390afd30db8502e1f
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2025-12-06 13:48:24 +0200

    nvim-tree: Don't open tree if nvim opened with file.

--- lua/plugins-config/nvim-tree.lua
+++ lua/plugins-config/nvim-tree.lua
@@ -38,8 +38,8 @@ require("nvim-tree").setup({
 local function open_nvim_tree()
 	if vim.fn.argc() == 0 then
 		vim.cmd("Startify")
+		require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
 	end
-	require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
 end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
